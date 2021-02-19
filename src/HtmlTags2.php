045f436de7d42109456121228eadf6b2bff826e5
Valeriy Turbanov <tur.4000@gmail.com>
2021-02-19 10:40:23 +0200

    lesson 4 start

---
+++ src/HtmlTags2.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\HtmlTags;
+
+use function Php\Pairs\Data\Lists\l;
+use function Php\Pairs\Data\Lists\head;
+use function Php\Pairs\Data\Lists\tail;
+use function Php\Pairs\Data\Lists\cons;
+use function Php\Pairs\Data\Lists\reverse;
+use function Php\Pairs\Data\Lists\isEmpty;
+use function Php\Html\Tags\HtmlTags\getName;
+use function Php\Html\Tags\HtmlTags\getValue;
+use function Php\Html\Tags\HtmlTags\node;
+use function Php\Html\Tags\HtmlTags\is;
+
+// BEGIN (write your solution here)
+function map()
+{}
+
+function mirror()
+{}
+// END
+
+function b2p($elements)
+{
+    if (isEmpty($elements)) {
+        return l();
+    }
+
+    $element = head($elements);
+    if (is('blockquote', $element)) {
+        $newElement = node('p', getValue($element));
+    } else {
+        $newElement = $element;
+    }
+
+    return cons($newElement, b2p(tail($elements)));
+}
