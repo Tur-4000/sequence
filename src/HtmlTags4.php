431d43e88f3e438a77d75a2847973a469b042d13
Turbanov Valeriy <tur.4000@gmail.com>
2021-02-20 10:21:16 +0200

    lesson 6

--- src/HtmlTags4.php
+++ src/HtmlTags4.php
@@ -11,12 +11,42 @@ use function Php\Html\Tags\HtmlTags\is;
 // BEGIN (write your solution here)
 function reduce($html, callable $func, $acc)
 {
-    return null;
+    $iter = function ($dom, $acc) use (&$iter, $func) {
+        if (isEmpty($dom)) {
+            return $acc;
+        }
+
+        $node = head($dom);
+        $newAcc = $func($node, $acc);
+        return $iter(tail($dom), $newAcc);
+    };
+
+    return $iter($html, $acc);
+
+/*
+ * решение учителя
+if (isEmpty($elements)) {
+        return $acc;
+    }
+
+return reduce(tail($elements), $func, $func(head($elements), $acc));
+*/
 }
 
 function emptyTagsCount(string $tagName, $html)
 {
-    return null;
+    return reduce($html, function ($tag, $acc) use ($tagName) {
+        return (is($tagName, $tag) && getValue($tag) === '') ? $acc + 1 : $acc;
+    }, 0);
+
+/*
+ * решение учителя
+$predicate = function ($element) use ($tagName) {
+        return is($tagName, $element) && getValue($element) === '';
+    };
+
+return reduce($elements, fn($element, $acc) => $predicate($element) ? $acc + 1 : $acc, 0);
+*/
 }
 // END
 
