090e49b3917b69e9b29a0d0effc46fbec16ae651
Turbanov Valeriy <tur.4000@gmail.com>
2021-02-21 00:07:56 +0200

    lesson 7 -- end

--- src/HtmlTags7.php
+++ src/HtmlTags7.php
@@ -12,11 +12,25 @@ use function Php\Html\Tags\HtmlTags\reduce;
 // BEGIN (write your solution here)
 function extractHeaders($html)
 {
-    return $html;
+    $headers = filter($html, fn($node) => is('h2', $node));
+
+    return map($headers, fn($node) => node('p', getValue($node)));
 }
 
 function wordsCount(string $tagName, string $word, $html): int
 {
-    return 0;
+    $filtered = filter($html, fn($node) => is($tagName, $node));
+
+    return reduce(
+        $filtered,
+        fn($node, $acc) => $acc + \substr_count(getValue($node), $word),
+        0
+    );
+/*
+ * решение учителя
+$filtered = filter($elements, fn($element) => is($tagName, $element));
+$values = map($filtered, fn($element) => getValue($element));
+return reduce($values, fn($text, $acc) => substr_count($text, $word) + $acc, 0);
+*/
 }
 // END
